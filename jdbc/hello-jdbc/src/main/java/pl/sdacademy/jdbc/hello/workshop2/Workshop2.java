6b7c94a1ee791af2257b53c8695ea4e6c37922ca
wkargul <wojciech.kargul@gmail.com>
2021-05-13 20:33:57 +0200

    After workshop2

--- jdbc/hello-jdbc/src/main/java/pl/sdacademy/jdbc/hello/workshop2/Workshop2.java
+++ jdbc/hello-jdbc/src/main/java/pl/sdacademy/jdbc/hello/workshop2/Workshop2.java
@@ -1,5 +1,7 @@
 package pl.sdacademy.jdbc.hello.workshop2;
 
+import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,6 +16,20 @@ public class Workshop2 {
     }
 
     private static List<String> getCountries(String continent) {
-        throw new UnsupportedOperationException("TODO");
+        List<String> result = new LinkedList<>();
+        try (Connection connection
+                     = DriverManager.getConnection("jdbc:mysql://localhost:6306/world", "root", "example")) {
+            try (PreparedStatement preparedStatement
+                         = connection.prepareStatement("SELECT * FROM country WHERE continent = ? ORDER BY name;")) {
+                preparedStatement.setString(1, continent);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    result.add(resultSet.getString("Name"));
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
     }
 }
