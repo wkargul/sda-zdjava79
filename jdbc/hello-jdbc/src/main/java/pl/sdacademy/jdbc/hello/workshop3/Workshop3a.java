acf013074cbdac2224091e722c225ed9ddde03dd
wkargul <wojciech.kargul@gmail.com>
2021-05-13 20:53:13 +0200

    After workshop3

--- jdbc/hello-jdbc/src/main/java/pl/sdacademy/jdbc/hello/workshop3/Workshop3a.java
+++ jdbc/hello-jdbc/src/main/java/pl/sdacademy/jdbc/hello/workshop3/Workshop3a.java
@@ -3,6 +3,7 @@ package pl.sdacademy.jdbc.hello.workshop3;
 import pl.sdacademy.jdbc.hello.common.City;
 
 import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -18,6 +19,26 @@ public class Workshop3a {
     }
 
     private static List<City> getCities(String countryCode) throws SQLException {
-        throw new UnsupportedOperationException("TODO");
+        List<City> result = new LinkedList<>();
+        try (Connection connection
+                     = DriverManager.getConnection("jdbc:mysql://localhost:6306/world", "root", "example")) {
+            try (PreparedStatement preparedStatement
+                         = connection.prepareStatement("SELECT * FROM city WHERE CountryCode = ? ORDER BY name;")) {
+                preparedStatement.setString(1, countryCode);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    City city = new City(
+                            resultSet.getString("Name"),
+                            resultSet.getString("CountryCode"),
+                            resultSet.getString("District"),
+                            resultSet.getInt("Population")
+                            );
+                    result.add(city);
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return result;
     }
 }
