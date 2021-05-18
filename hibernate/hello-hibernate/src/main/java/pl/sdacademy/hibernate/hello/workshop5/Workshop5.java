cd6e5fd69873bf50d9623f3aa93c9ec28476261b
Kinga Izdebska <kinga.izdebska@gmail.com>
2021-05-18 21:04:50 +0200

    ex5

--- hibernate/hello-hibernate/src/main/java/pl/sdacademy/hibernate/hello/workshop5/Workshop5.java
+++ hibernate/hello-hibernate/src/main/java/pl/sdacademy/hibernate/hello/workshop5/Workshop5.java
@@ -1,5 +1,8 @@
 package pl.sdacademy.hibernate.hello.workshop5;
 
+import pl.sdacademy.hibernate.hello.common.City;
+
+import javax.persistence.*;
 import java.util.*;
 
 public class Workshop5 {
@@ -21,6 +24,23 @@ public class Workshop5 {
     }
 
     public static Map<String,List<String>> getCities(String... codes) {
-        throw new UnsupportedOperationException("TODO");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("HelloHibernatePU");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        String query =
+                "SELECT ci.name as cityName, ci.country.name as countryName FROM City ci  JOIN  ci.country where ci.country.code in :countryCodes ORDER BY ci.name";
+
+        try {
+            TypedQuery<Tuple> typedQuery = entityManager.createQuery(query, Tuple.class);
+            List<Tuple> listTuple =  typedQuery.setParameter("countryCodes",Set.of(codes)).getResultList();
+            Map<String,List<String>> tupleMap = new LinkedHashMap<>();
+            for (Tuple tuple:listTuple) {
+                String cityName = tuple.get("cityName",String.class);
+                String countryName = tuple.get("countryName",String.class);
+                tupleMap.computeIfAbsent(countryName, x -> new LinkedList<>()).add(cityName);
+            }
+            return tupleMap;
+        } finally {
+            entityManagerFactory.close();
+        }
     }
 }
