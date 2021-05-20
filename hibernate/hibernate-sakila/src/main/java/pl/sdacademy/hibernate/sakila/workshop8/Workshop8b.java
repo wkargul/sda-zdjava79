81a06cbbd4941fff3f137a530a4d16815c836881
Tomasz Majbaum <tomasz.majbaum@gmail.com>
2021-05-20 18:24:44 +0200

    Hibernate Sakila, workshop8b

--- hibernate/hibernate-sakila/src/main/java/pl/sdacademy/hibernate/sakila/workshop8/Workshop8b.java
+++ hibernate/hibernate-sakila/src/main/java/pl/sdacademy/hibernate/sakila/workshop8/Workshop8b.java
@@ -36,6 +36,27 @@ public class Workshop8b {
     }
 
     public static List<Country> findAllCountries() {
-        throw new UnsupportedOperationException("TODO");
+        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("SakilaPU");
+        final EntityManager em = emf.createEntityManager();
+
+        try {
+            //TypedQuery<Country> countriesQuery = em.createQuery("select c from Country c", Country.class); //nie załaduje danych
+
+            //nie pozwala na fetch zagnieżdżonych list
+            TypedQuery<Country> countriesQuery = em.createQuery(
+                "select c from Country c join fetch c.cities cit join fetch cit.addresses order by c.name",
+                Country.class
+            );
+
+            //najlepiej przebudować zadanie i unikać ładowania List<Country> w taki sposób
+            //final TypedQuery<Tuple> countriesQuery = em.createQuery("select c.name as country_name, cit.name as city_name, a.address from Country c join c.cities cit join cit.addresses a", Tuple.class);
+
+            List<Country> resultList = countriesQuery.getResultList();
+
+            return resultList;
+        }
+        finally {
+            emf.close();
+        }
     }
 }
