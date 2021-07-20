f79239af2f7758b97709227cec6bba08f5fc24c3
Tomasz Majbaum <tomasz.majbaum@gmail.com>
2021-07-20 19:32:42 +0200

    spring advanced workshop4: Environment

--- spring/advanced/src/main/java/pl/sdacademy/java/spring/advanced/workshop4/FooTest.java
+++ spring/advanced/src/main/java/pl/sdacademy/java/spring/advanced/workshop4/FooTest.java
@@ -2,22 +2,27 @@ package pl.sdacademy.java.spring.advanced.workshop4;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 @Component
 public class FooTest {
     private static final Logger logger = LoggerFactory.getLogger(FooTest.class);
 
     private final Foo foo;
+    private final Environment environment;
 
-    public FooTest(Foo foo) {
+    public FooTest(Foo foo, Environment environment) {
         this.foo = foo;
+        this.environment = environment;
     }
 
     @PostConstruct
     public void ready() {
+        logger.info("Aktywne profile: " + Arrays.toString(environment.getActiveProfiles()));
         logger.info(foo.getMessage());
     }
 }
