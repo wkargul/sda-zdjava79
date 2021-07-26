eb82be3977a5587b3b5340f64eb8d5fd3ec16175
Tomasz Majbaum <tomasz.majbaum@gmail.com>
2021-07-26 19:21:52 +0200

    hello spring security: workshop 1 stub

---
+++ spring/hello-security/src/main/java/pl/sdacademy/java/spring/hellosecurity/workshop1to3/Workshop1to3Application.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.java.spring.hellosecurity.workshop1to3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop1to3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Workshop1to3Application.class, args);
+    }
+
+}
